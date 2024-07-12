1c9394126b9098514f9f3f075fd7aff62710bf7c
William <69966592+WilliamTheDev@users.noreply.github.com>
2024-07-12 18:59:28 +1000

    What's git bash??

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 import json
-import os
 
-from scripts import decryption, encryption
+from scripts import crypt 
+from scripts import database
+from getpass import getpass
 
-dataBasePath = 'database/accounts.json'
+# global variables
+dataBasePath = './database/accounts.json'
 
-masterpassword = "1"
+def add_account(account: dict, master: str) -> None:
+    # reads database & gets the number of accounts
+    data = database.read(dataBasePath)
+    numAccount = len(data['accounts']) + 1
+    # encrypts the account data & deletes the unencrypted data
+    encryptedAccount = crypt.encrypt(str(account), master)
+    del account
 
-def read(database):
-    with open(database, "r") as file:
-        data = json.load(file)
-    return data
+    # Writes the encrypted data 
+    data['accounts'][numAccount] = encryptedAccount
+    database.write(dataBasePath, data)
 
-def encrypt(account):
-    data = read(dataBasePath)
-    encrypted = encryption.main(account[1], masterpassword)
-    newpassword = {
-        'label': account[2],
-        'password': encrypted,
-    }
-    data['accounts'][account[0]] = newpassword
-    print(data)
-    with open(dataBasePath, "w") as outfile:
-        json.dump(data, outfile)
 
-def decrypt(database):
-    data = read(dataBasePath)
-    try:
-        for account in data['accounts']:
-            decrypted = decryption.main(data['accounts'][account]["password"], masterpassword)
-            print(account, decrypted)
-    except:
-        print("incorrect password: gfys")
-    
-decrypt(dataBasePath)
-# encrypt(account1)
-# encrypt(account2)
-# encrypt(account3)
+def show_accounts() -> None:
+    # reads database
+    data = database.read(dataBasePath)
+    for account in data['accounts']:
+        decrypt = (crypt.decrypt(data['accounts'][account], master))
+        accountDecode = decrypt.decode('utf-8')  
+        account = json.loads(accountDecode.replace("'", '"'))  
+        print(account['name'], account['label'], account['password'])
+        
+master = getpass("enter master password: ")
+#add_account(account, master)
+show_accounts()
