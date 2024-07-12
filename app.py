d9561e09cd89be80f8a93fab4016bd35fc0f9b4b
William <69966592+WilliamTheDev@users.noreply.github.com>
2024-07-12 21:26:46 +1000

    I don't even know

--- app.py
+++ app.py
@@ -5,30 +5,45 @@ from scripts import database
 from getpass import getpass
 
 # global variables
-dataBasePath = './database/accounts.json'
+DBPATH = './database/accounts.json'
 
-def add_account(account: dict, master: str) -> None:
+def create_account() -> dict:
+    # Creates and fills a new account  
+    newAccount = {}
+    newAccount['name'] = input("Enter the new service name: ")
+    newAccount['label'] = input("Enter a label name: ")
+    newAccount['password'] = getpass("Enter the service password: ")
+    return newAccount
+
+# adds a new account to DB
+def add_account(newAccount: dict, masterPass: str) -> None:
     # reads database & gets the number of accounts
-    data = database.read(dataBasePath)
-    numAccount = len(data['accounts']) + 1
+    dbData = database.read(DBPATH)
+    accountID = len(dbData['accounts']) + 1
     # encrypts the account data & deletes the unencrypted data
-    encryptedAccount = crypt.encrypt(str(account), master)
-    del account
-
+    encryptedData = crypt.encrypt(str(newAccount), masterPass)
+    del newAccount
+ 
     # Writes the encrypted data 
-    data['accounts'][numAccount] = encryptedAccount
-    database.write(dataBasePath, data)
+    dbData['accounts'][accountID] = encryptedData
+    database.write(DBPATH, dbData)
 
-
-def show_accounts() -> None:
+# Shows all accounts
+def show_accounts(masterPass) -> None:
     # reads database
-    data = database.read(dataBasePath)
-    for account in data['accounts']:
-        decrypt = (crypt.decrypt(data['accounts'][account], master))
-        accountDecode = decrypt.decode('utf-8')  
+    dbData = database.read(DBPATH)
+    if len(dbData['accounts']) < 0:  
+        print("the database is empty")
+        return 
+
+    #Decrypts the account and prints it 
+    for account in dbData['accounts']:
+        accountDecrypted = crypt.decrypt(dbData['accounts'][account], masterPass)
+        accountDecode = accountDecrypted.decode('utf-8')  
         account = json.loads(accountDecode.replace("'", '"'))  
         print(account['name'], account['label'], account['password'])
-        
-master = getpass("enter master password: ")
-#add_account(account, master)
-show_accounts()
+
+masterPass = input("Enter the master password: ")
+show_accounts(masterPass)
+add_account(create_account(), masterPass)
+show_accounts(masterPass)
