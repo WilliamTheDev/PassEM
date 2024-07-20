42196382d15143cc8c3373c28798c5c02f9f8906
William <69966592+WilliamTheDev@users.noreply.github.com>
2024-07-20 16:47:11 +1000

    Update crypt.py

--- scripts/crypt.py
+++ scripts/crypt.py
@@ -1,46 +1,47 @@
 import hashlib
+import json 
 
 from base64 import b64encode, b64decode
 from Cryptodome.Cipher import AES
-from Cryptodome.Random import get_random_bytes
+from Cryptodome.Random import get_random_bytes as getRandomBytes
 
 # encrypts 
-def encrypt(plain_text: str, password: str) -> list:
+def encrypt(account: str, masterPass: str) -> list:
     # generate a random salt
-    salt = get_random_bytes(AES.block_size)
+    salt = getRandomBytes(AES.block_size)
 
     # use the Scrypt KDF to get a private key from the password
-    private_key = hashlib.scrypt(password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+    privateKey = hashlib.scrypt(masterPass.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
 
     # create cipher config
-    cipher_config = AES.new(private_key, AES.MODE_GCM)
+    cipherConfig = AES.new(privateKey, AES.MODE_GCM)
 
     # return a dictionary with the encrypted text
-    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
+    encryptedText, tag = cipherConfig.encrypt_and_digest(bytes(account, 'utf-8'))
     return {
-        'service': b64encode(cipher_text).decode('utf-8'),
+        'account': b64encode(encryptedText).decode('utf-8'),
         'salt': b64encode(salt).decode('utf-8'),
-        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
+        'nonce': b64encode(cipherConfig.nonce).decode('utf-8'),
         'tag': b64encode(tag).decode('utf-8')
     }
 
 # Decrypts 
-def decrypt(enc_dict: list, password: str) -> str:
+def decrypt(encryptedAccount: list, password: str) -> dict:
     # decode the dictionary entries from base64
-    salt = b64decode(enc_dict['salt'])
-    cipher_text = b64decode(enc_dict['service'])
-    nonce = b64decode(enc_dict['nonce'])
-    tag = b64decode(enc_dict['tag'])
+    account = b64decode(encryptedAccount['account'])
+    salt = b64decode(encryptedAccount['salt'])
+    nonce = b64decode(encryptedAccount['nonce'])
+    tag = b64decode(encryptedAccount['tag'])
 
-    # generate the private key from the password and salt
-    private_key = hashlib.scrypt(
-        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+    # generate the private key & cipher config
+    privateKey = hashlib.scrypt(password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+    cipherConfig = AES.new(privateKey, AES.MODE_GCM, nonce=nonce)
 
-    # create the cipher config
-    cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+    # decrypts the account 
+    decryptedAccount = cipherConfig.decrypt_and_verify(account, tag)
+    decodeAccount = decryptedAccount.decode('utf-8') 
+    jsonAccount = json.loads(decodeAccount.replace("'", '"'))  
 
-    # decrypt the cipher text
-    decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+    return jsonAccount 
 
-    return decrypted
 
