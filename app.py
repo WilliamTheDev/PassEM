b769f61acec68124b9d50937a2096dba8bbb2a06
William <69966592+WilliamTheDev@users.noreply.github.com>
2024-07-20 16:46:01 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,96 @@
-import json
 
-from scripts import crypt 
-from scripts import database
+from scripts import database as data_base
+from scripts import crypt
+
+from random import choice 
+from string import digits, ascii_letters, punctuation
 from getpass import getpass
+from prompt_toolkit import prompt
+from json import dumps
 
 # global variables
 DBPATH = './database/accounts.json'
+IDLENGTH = 32
 
-def create_account() -> dict:
-    # Creates and fills a new account  
-    newAccount = {}
-    newAccount['name'] = input("Enter the new service name: ")
-    newAccount['label'] = input("Enter a label name: ")
-    newAccount['password'] = getpass("Enter the service password: ")
-    return newAccount
+def generate_account_id(length: int) -> str:
+    characters = digits + ascii_letters
+    accountId = ""
+    for _ in range(length):
+        accountId += choice(characters)
+    return accountId
 
-# adds a new account to DB
-def add_account(newAccount: dict, masterPass: str) -> None:
-    # reads database & gets the number of accounts
-    dbData = database.read(DBPATH)
-    accountID = len(dbData['accounts']) + 1
-    # encrypts the account data & deletes the unencrypted data
-    encryptedData = crypt.encrypt(str(newAccount), masterPass)
-    del newAccount
- 
-    # Writes the encrypted data 
-    dbData['accounts'][accountID] = encryptedData
-    database.write(DBPATH, dbData)
 
-# Shows all accounts
-def show_accounts(masterPass) -> None:
-    # reads database
-    dbData = database.read(DBPATH)
-    if len(dbData['accounts']) < 1:  
-        print("the database is empty")
-        return 
+def generate_password(length: int) -> str:
+    characters = digits + ascii_letters + punctuation 
+    password = ""
+    for _ in range(length):
+        password += choice(characters)
+    return password
 
-    #Decrypts the account and prints it 
-    for account in dbData['accounts']:
-        accountDecrypted = crypt.decrypt(dbData['accounts'][account], masterPass)
-        accountDecode = accountDecrypted.decode('utf-8')  
-        account = json.loads(accountDecode.replace("'", '"'))  
-        print(account['name'], account['label'], account['password'])
 
-masterPass = input("Enter the master password: ")
-show_accounts(masterPass)
-add_account(create_account(), masterPass)
-show_accounts(masterPass)
+def password_check() -> str:
+    while True:
+        passNew = getpass("Enter the new password: ")
+        passCon = getpass("Confrim the new password: ")
+        if passNew == passCon:
+            return passNew
+        print("Passwords do not match. Try again")
+
+
+def load_db(dbpath: str, masterPass: str) -> dict:
+    dbData = data_base.read(dbpath)
+
+    if len(dbData['accounts']) == 0:
+        print(f"No accounts found in the database '{dbpath}'")
+        return None
+
+    decryptedData = {}
+    try: 
+        for ID in dbData['accounts']:
+            decryptedAccount = crypt.decrypt(dbData['accounts'][ID], masterPass)
+            decryptedData[ID] = decryptedAccount
+    except Exception as e:
+        print(f"Failed to decrypt accounts: {e}")
+        exit()
+
+    return decryptedData
+
+
+def edit_account(dbpath: str, accountId: str, masterPass: str) -> None:
+    dbData = data_base.read(dbpath)
+
+    if accountId not in dbData['accounts']:
+        print(f"Account with ID '{accountId}' not found in the database.")
+        return
+    
+    encryptedAccount = dbData['accounts'][accountId]
+    decryptedAccount = crypt.decrypt(encryptedAccount, masterPass)
+
+    editedAccount = {
+        'name': prompt("Edit The account Name: ", default=decryptedAccount['name']),
+        'label': prompt("Edit The account Labels: ", default=decryptedAccount['label']),
+        'password': password_check
+    }
+
+    encryptAccount = crypt.encrypt(dumps(editedAccount), masterPass)
+    dbData['accounts'][accountId] = encryptAccount
+    data_base.write(dbpath, dbData)
+
+
+def add_account(dbpath: str, masterPass: str) -> None:
+    newAccountId = generate_account_id(IDLENGTH)
+    newAccount = {
+        'name': input("Enter the new account name: "),
+        'label': input("Enter a label name: "),
+        'password': password_check
+    }
+    encryptedAccount = crypt.encrypt(dumps(newAccount), masterPass)
+
+    dbData = data_base.read(dbpath)
+    dbData['accounts'][newAccountId] = encryptedAccount
+    data_base.write(dbpath, dbData)
+
+
+masterPass = getpass(F"Enter the master password for '{DBPATH}': ")
+accounts = data_base.load_db(DBPATH, masterPass)
+print(accounts)
